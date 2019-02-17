7570e7ed204b119ccd0fb5f00c453933935b7d2b
antoine prudhomme <antoine.prudhomme@spendesk.com>
2019-02-17 22:43:39 +0100

    add cron and main function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 require('dotenv').config();
+const cron = require('node-cron');
+
+const { getRandomUsers } = require('./services/user');
+const {
+  createInstantMessageChannel,
+  postMessage,
+} = require('./services/channel');
+
+const coffeeMessage = "You've been selected for a coffee break :coffee:\nLet's meet in the snack room in 5 minutes :)";
+
+const organiseCoffeeBreak = async () => {
+  const users = await getRandomUsers();
+  const channel = await createInstantMessageChannel(users);
+  postMessage(channel, coffeeMessage);
+};
 
 async function main() {
-
+  cron.schedule(process.env.COFFEE_TIME_CRON, () => {
+    organiseCoffeeBreak();
+  });
 }
 
 main();
